a75edbddcf99fc9671878969e66d39054605ff90
Paul Stewart <pstewart@mercurypay.com>
2013-03-26 11:40:10 -0600

    adding recent update to ui

---
+++ WSPAPIPrototype/frmMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WSPAPIPrototype
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPayment_Click(object sender, EventArgs e)
+        {
+                Payment payment = new Payment();
+            payment.Process();
+        }
+
+        private void btnHostedCheckout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoyalty_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog(); // Shows Form2
+        }
+
+        private void btnGift_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
